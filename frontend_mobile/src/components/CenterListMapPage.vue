bf848d93839f0b7bfd26b19f3f87d6209e391d31
jjangs <jjangs85@gmail.com>
2021-09-18 16:01:43 +0900

    210918_8

--- frontend_mobile/src/components/CenterListMapPage.vue
+++ frontend_mobile/src/components/CenterListMapPage.vue
@@ -9,11 +9,11 @@
         <a href="#" data-toggle-theme class="header-icon header-icon-3 show-on-theme-light"><i class="fas fa-moon"></i></a>
     </div>
 
-    <div class="map-filter text-center">
-        <a id="btn-filter1" class="btn btn-sm rounded-0 bg-highlight">스쿠버다이빙</a>
-        <a id="btn-filter2" class="btn btn-sm rounded-0 bg-highlight-light">프리다이빙</a>
-    </div>
     <div class="page-content pb-0" style="height: 100% !important;">
+        <div class="wedive-action text-center" style="z-index: 99;">
+            <button data-menu="menu-filter" class="action-filter"><span>필터</span></button>
+            <a href="/center_map" class="action-map"><span>지도</span></a>
+        </div>
         <div id="map" style="height: 100% !important;position: inherit !important;"></div>
         
         <div class="map-box hide">
@@ -257,5 +257,12 @@ export default {
 #btn-filter2 {padding: 5px 10px !important;margin-left: -3px;border-radius: 0px 20px 20px 0px !important;border: 1px solid #091a42;}
 #btn-filter2:before {clear: both;content: "";display: inline-block;margin-right: 6px;width: 14px;height: 14px;background-size: 14px 14px;background-repeat: no-repeat;background-image: url(/static/images/icon_freediving.png);text-indent: -9999px;vertical-align: middle;}
 
+
+.wedive-action {position: fixed;background-color: #1d397c;height:36px;border-radius: 18px;left: 50%!important;bottom: 90px;-webkit-box-shadow: 3px 2px 6px 0 rgb(0 0 0 / 20%);box-shadow: 3px 2px 6px 0 rgb(0 0 0 / 20%);-webkit-transform: translateX(-50%);transform: translateX(-50%);}
+.wedive-action:before {display: block;clear: both;content: "";position: absolute;top: 9px;left: 50%;width: 1px;height: 16px;background-color: #fff;}
+.action-filter {display: block;float: left;width: 84px;height: 35px;font-size: 14px;color: #d1d2d3;line-height: 35px;}
+.action-filter:before {clear: both;content: "";display: inline-block;margin-right: 6px;width: 14px;height: 14px;background-size: 14px 14px;background-repeat: no-repeat;background-image: url(/static/images/icon_filter.png);text-indent: -9999px;vertical-align: middle;}
+.action-map {position: relative;float: right;width: 74px;height: 35px;font-size: 14px;color: #d1d2d3;line-height: 35px;}
+.action-map:before {clear: both;content: "";display: inline-block;margin-right: 6px;width: 14px;height: 14px;background-size: 14px 14px;background-repeat: no-repeat;background-image: url(/static/images/icon_map.png);text-indent: -9999px;vertical-align: middle;}
 
 </style>
