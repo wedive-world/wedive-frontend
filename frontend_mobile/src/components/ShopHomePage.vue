64fa237aa6ae7dfb7de2fbff20a9dcb0f7a18e6d
jjangs <jjangs85@gmail.com>
2021-09-27 18:15:38 +0900

    210927_7

--- frontend_mobile/src/components/ShopHomePage.vue
+++ frontend_mobile/src/components/ShopHomePage.vue
@@ -354,7 +354,7 @@ export default {
     if(preloader){preloader.classList.add('preloader-hide');}
     
     $("body").css("background-color", "#fff");
-    $(".logo-image").eq(1).attr("src","/static/images/logo-shop.svg")
+    $(".logo-image").eq(1).attr("src","/static/images/logo-shop2.svg")
 
     setTimeout(function() {
       window.onscroll = function() {myFunction()};
