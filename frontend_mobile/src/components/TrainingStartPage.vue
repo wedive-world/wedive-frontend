16ec6d2fbaf96b1bd75a0af76d22c7180ae77da5
jjangs <jjangs85@gmail.com>
2021-10-02 08:21:26 +0900

    211002_1

--- frontend_mobile/src/components/TrainingStartPage.vue
+++ frontend_mobile/src/components/TrainingStartPage.vue
@@ -3,56 +3,56 @@
     
     <div class="card card-style p-3 mb-3">
       <div class="progress1 text-center" style="height:170px;"></div>
-      <div style="position: absolute;right: 20px;bottom: 14px;"><span class="font-noto font-14 pe-2 color-secondary">소요시간</span><span class="font-stencil font-18" style="font-style: italic;">02:53</span></div>
+      <div style="position: absolute;right: 20px;bottom: 14px;"><span class="font-noto font-14 mb-0 color-secondary" style="display: block;">소요시간</span><span class="font-exo2 font-20 mt-n1" style="display: block;">02:53</span></div>
     </div>
 
     <div class="card card-style p-3 mb-3">
       <table class="table table-borderless text-center rounded-sm shadow-l bg-white font-noto font-15" style="overflow: hidden;">
         <thead>
             <tr class="bg-highlight">
-                <th scope="col" class="color-white font-500" style="width: 84px;">회차</th>
+                <th scope="col" class="color-white font-500" style="width: 80px;">회차</th>
                 <th scope="col" class="color-white font-500">호흡</th>
                 <th scope="col" class="color-white font-500">참기</th>
             </tr>
         </thead>
         <tbody>
             <tr class="border-bottom">
-                <th class="" scope="row">1</th>
+                <th class="" scope="row" style="width: 80px;">1</th>
                 <td><div>2:00</div></td>
                 <td><div>0:40</div><div class="td-abs">0:32</div></td>
             </tr>
             <tr class="border-bottom">
-                <th class="" scope="row">2</th>
+                <th class="" scope="row" style="width: 80px;">2</th>
                 <td><div class="td-active">1:45</div></td>
                 <td><div>0:40</div></td>
             </tr>
             <tr class="border-bottom">
-                <th class="" scope="row">3</th>
+                <th class="" scope="row" style="width: 80px;">3</th>
                 <td><div>1:30</div></td>
                 <td><div>0:40</div></td>
             </tr>
             <tr class="border-bottom">
-                <th class="" scope="row">4</th>
+                <th class="" scope="row" style="width: 80px;">4</th>
                 <td><div>1:15</div></td>
                 <td><div>0:40</div></td>
             </tr>
             <tr class="border-bottom">
-                <th class="" scope="row">5</th>
+                <th class="" scope="row" style="width: 80px;">5</th>
                 <td><div>1:00</div></td>
                 <td><div>0:40</div></td>
             </tr>
             <tr class="border-bottom">
-                <th class="" scope="row">6</th>
+                <th class="" scope="row" style="width: 80px;">6</th>
                 <td><div>0:45</div></td>
                 <td><div>0:40</div></td>
             </tr>
             <tr class="border-bottom">
-                <th class="" scope="row">7</th>
+                <th class="" scope="row" style="width: 80px;">7</th>
                 <td><div>0:30</div></td>
                 <td><div>0:40</div></td>
             </tr>
             <tr>
-                <th class="" scope="row">8</th>
+                <th class="" scope="row" style="width: 80px;">8</th>
                 <td><div>0:15</div></td>
                 <td><div>0:40</div></td>
             </tr>
@@ -60,12 +60,12 @@
     </table>
     </div>
 
-    <div class="card card-style">
+    <div class="card card-style pb-3">
       <div class="d-flex">
-        <div class="flex-grow-1 text-center mt-3"><img src="/static/images/icon_giveup.svg" width="50"/><p class="mb-3 mt-2">참기 실패</p></div>
-        <div class="flex-grow-1 text-center mt-3 border-left"><img src="/static/images/icon_timer.svg" width="50"/><p class="mb-3 mt-2">참기 +5초</p></div>
-        <div class="flex-grow-1 text-center mt-3 border-left"><img src="/static/images/icon_pause.svg" width="50"/><p class="mb-3 mt-2">일시중지</p></div>
-        <div class="flex-grow-1 text-center mt-3 border-left"><img src="/static/images/icon_stop.svg" width="50"/><p class="mb-3 mt-2">종료</p></div>
+        <div class="flex-grow-1 text-center mt-3"><img src="/static/images/icon_giveup.svg" width="40"/><p class="mb-0 mt-2">참기 실패</p></div>
+        <div class="flex-grow-1 text-center mt-3 border-left"><img src="/static/images/icon_timer.svg" width="40"/><p class="mb-0 mt-2">참기 +5초</p></div>
+        <div class="flex-grow-1 text-center mt-3 border-left"><img src="/static/images/icon_pause.svg" width="40"/><p class="mb-0 mt-2">일시중지</p></div>
+        <div class="flex-grow-1 text-center mt-3 border-left"><img src="/static/images/icon_stop.svg" width="35" style="margin-top:2.5px;"/><p class="mb-0 mt-2">종료</p></div>
       </div>
     </div>
 
@@ -109,7 +109,7 @@ export default {
 
         $(".circle-progress-circle").css("stroke-width", "2px");
         $(".circle-progress-text-value").text("01:32");
-        $(".circle-progress-text-value").css("font-family", "Saira Stencil One");
+        $(".circle-progress-text-value").css("font-family", "'Exo 2', sans-serif");
         $(".circle-progress-text-value").css("font-size", "23px");
         $(".circle-progress-text-value").css("fill", "black");
         $(".circle-progress-text-value").attr("x", "48");
@@ -149,9 +149,10 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Saira+Stencil+One&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Exo+2:ital,wght@1,700&display=swap');
 
-.font-stencil {font-family: 'Saira Stencil One';}
+
+.font-exo2 {font-family: 'Exo 2', sans-serif;}
 .font-noto {font-family: 'Noto Sans Korean';}
 .bl-1 {border-left: 1px solid rgba(0, 0, 0, 0.08) !important;}
 .border-bottom {border-bottom: 1px solid rgba(0, 0, 0, 0.08) !important;}
