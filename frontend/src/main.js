ca5b2397d689350e8fd28b0fad86146d73e57436
Mark Yun <only.hyeok@gmail.com>
2021-10-04 15:11:57 +0900

    added dotenv

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -8,6 +8,7 @@ import ApolloClient from 'apollo-boost'
 
 const GRAPHQL_URL = process.env.API_PATH || 'http://localhost:4000/graphql'
 
+require('dotenv').config();
 console.log(`==============================Env Information==============================`)
 Object.keys(process.env)
   .forEach(key => {
