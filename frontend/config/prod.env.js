59785bb948cb001e8a0d609a47c2b1659d08ac5b
Mark Yun <only.hyeok@gmail.com>
2021-10-04 20:46:13 +0900

    added

--- frontend/config/prod.env.js
+++ frontend/config/prod.env.js
@@ -1,4 +1,9 @@
 'use strict'
+
+const dotenv = require('dotenv');
+const env = dotenv.config().parsed;
+
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  VUE_APP_API_PATH: env.VUE_APP_API_PATH
 }
