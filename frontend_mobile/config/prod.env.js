631f7229d6d50b26a08ac55abb6dcce50d84d8cb
Mark Yun <only.hyeok@gmail.com>
2021-10-04 21:28:00 +0900

    added mobile env

--- frontend_mobile/config/prod.env.js
+++ frontend_mobile/config/prod.env.js
@@ -1,4 +1,9 @@
 'use strict'
+
+const dotenv = require('dotenv');
+const env = dotenv.config().parsed;
+
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  VUE_APP_API_PATH: JSON.stringify(env.VUE_APP_API_PATH)
 }
