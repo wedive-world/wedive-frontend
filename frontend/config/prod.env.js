3df6e0948748dc941b18879012fbc75e637f7cb8
Mark Yun <only.hyeok@gmail.com>
2021-10-04 21:10:49 +0900

    added log

--- frontend/config/prod.env.js
+++ frontend/config/prod.env.js
@@ -3,6 +3,10 @@
 const dotenv = require('dotenv');
 const env = dotenv.config().parsed;
 
+console.log(`:::::::::::::::prod.env:::::::::::::`)
+console.log(JSON.stringify(env))
+console.log(`:::::::::::::::prod.env:::::::::::::`)
+
 module.exports = {
   NODE_ENV: '"production"',
   VUE_APP_API_PATH: JSON.stringify(env.VUE_APP_API_PATH)
