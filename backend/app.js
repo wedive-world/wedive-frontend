58250620a4c389b96b0e788be09e57f833270f01
장성환 <jjangs85@gmail.com>
2021-11-14 19:51:27 +0900

    fix

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
+app.use(express.limit('10M'));
 
 
 var mainapp = connect();
