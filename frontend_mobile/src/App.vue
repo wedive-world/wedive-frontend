27528a50bdd9dadbae2e4beabbd6f0f2e62ce666
Mark Yun <only.hyeok@gmail.com>
2021-11-20 17:14:35 +0900

    added log

--- frontend_mobile/src/App.vue
+++ frontend_mobile/src/App.vue
@@ -80,6 +80,10 @@ export default {
   }
 }
 
+</script>
+
+<script type="text/javascript">
+
 const userAgent = navigator.userAgent.toLowerCase();
 
 if (userAgent.indexOf('android') !== -1) {
@@ -88,7 +92,6 @@ if (userAgent.indexOf('android') !== -1) {
 
 } else if (userAgent.indexOf('iphone') !== -1 || userAgent.indexOf('ipad') !== -1) {
   //ios
-
 } else { // 안드로이드, IOS 가 아닌 경우 (더 조건을 추가해서 처리해도 됨)
 }
 
