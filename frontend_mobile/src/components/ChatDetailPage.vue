146948599a3c2ac8d700f32876ff00e0f3195a96
장성환 <jjangs85@gmail.com>
2022-01-01 12:40:51 +0900

    fix

--- frontend_mobile/src/components/ChatDetailPage.vue
+++ frontend_mobile/src/components/ChatDetailPage.vue
@@ -226,9 +226,14 @@ export default {
       },
       is_emoji: function(newVal, oldVal) {
           if (newVal) {
+              $('#speech-content').css("height", (this.speechContentHeight-300) + "px")
               setTimeout(function() {
                 hideKeyboard();
+                console.log("aa")
+                $('#speech-content').scrollTop($('#speech-content')[0].scrollHeight);
               },100)
+          } else {
+              $('#speech-content').css("height", (this.speechContentHeight) + "px")
           }
       }
   },
@@ -249,6 +254,11 @@ export default {
     if (this.$route.query.footer && this.$route.query.footer == 'hide') {
       $("#footer-bar").hide();
     }
+    this.speechContentHeight = $("#speech-content").height();
+    setTimeout(function() {
+        $('#speech-content').scrollTop($('#speech-content')[0].scrollHeight);
+    },200);
+    
   },
   components: {
     
@@ -259,9 +269,10 @@ export default {
         var preloader = document.getElementById('preloader')
         if(preloader){preloader.classList.add('preloader-hide');}
     }, 500);
+    window.addEventListener('native.showkeyboard', this.keyboardShowHandler);
   },
   destroyed () {
-    
+    window.removeEventListener('native.showkeyboard', this.keyboardShowHandler);
   },
   data () {
     return {
@@ -274,6 +285,7 @@ export default {
         is_emoji_clicked: false,
         emoji_url: '',
         emoji_regex: /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g,
+        speechContentHeight: 0,
     }
   }, methods: {
     setData(chatData, roomId) {
@@ -281,6 +293,9 @@ export default {
         this.chatData.reverse();
         this.chatData.forEach(chat => chat.showAt = timeForToday(chat.createdAt))
         this.roomId = roomId;
+    },
+    keyboardShowHandler(event) {
+        $('#speech-content').scrollTop($('#speech-content')[0].scrollHeight);
     },
     async sendMessage() {
         if (this.sendDisable == false) {
