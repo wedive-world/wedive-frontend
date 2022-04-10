39e8895d0308ee48063095f3bb1f443da2f67a44
jjangs <jjangs85@gmail.com>
2022-04-10 20:58:17 +0900

    fix

--- frontend_mobile/src/components/InstructorCreatePage.vue
+++ frontend_mobile/src/components/InstructorCreatePage.vue
@@ -26,10 +26,63 @@
         <swiper-slide
             :key="0"
             :virtualIndex="0">
+                    <div id="slide0" class="card card-full pb-0 mb-0 border-bottom" style="height: calc( 100vh - 56px );">
+                        <div class="content mt-0 text-center font-noto">
+                            <img src="/static/images/assets/trainer.png" class="mt-5" width="50%"/>
+                            <p class="font-18 font-500 mt-3 mb-0">위다이브 강사에 함께해요.</p>
+                            <!--<p class="color-gray font-14 mb-0">다이빙 입문자를 만나는 가장빠른 방법입니다.</p>-->
+
+                            <div class="me-5 ms-5 mt-2 rounded-s color-gray" style="">준비 물품</div>
+                            <div class="bg-fade-yellow-light me-4 ms-4 mt-1 rounded-s color-black" style="">증명 사진, 라이센스, 자기소개, 교육상품</div>
+                        </div>
+                        <div>
+                            
+                        </div>
+
+
+                        <div class="p-2 pb-3 content mt-5 mb-2 font-noto">
+                            <div class="accordion" id="accordion-2">
+                                <div class="card card-style shadow-0 m-0 mb-1" style="border-radius: 8px;">
+                                    <button class="btn accordion-btn color-black no-effect" style="padding: 6px 2px 6px 2px;">
+                                        <div class="form-check icon-check">
+                                            <input class="form-check-input" type="checkbox" value="" id="check_agree" v-model="check_agree">
+                                            <label class="form-check-label font-noto font-15" for="check_agree">개인정보 취급방침 동의</label>
+                                            <i class="icon-check-1 far fa-square color-gray-dark font-18"></i>
+                                            <i class="icon-check-2 far fa-check-square font-18 color-highlight"></i>
+                                        </div>
+                                    </button>
+
+                                    <div class="bg-theme">
+                                        <div class="" style="max-height: 160px;overflow-y: auto;border-radius: 10px;">
+                                            <p class="agreement_content pe-3 ps-3 color-gray" style="background: #fafbfc;">
+                                                &lt;개인정보 수집 동의&gt;<br><br>
+                                                1. 기본수집항목: [필수] 위다이브 아이디, 이름, (휴대)전화번호, 이메일 주소, 사진, 다이빙 라이센스, 강사 경력<br>※ 추가 수집하는 필수항목<br>- 배송, 방문 등이 필요한 상품 구매 시 : 주소<br>- 해외 여행 관련 상품 구매 시 : 여권상 영문명, 여권번호 끝 4자리, 성별, 생년월일, 이메일주소, 카카오톡ID, 동행 아동정보(여권상 영문명, 생년월일, 신장)<br>- 병원을 이용하는 경우: 생년월일 (병원 재진 시 이전 진료기록 조회를 위해 예약자명, 생년월일, 전화번호가 수집될 수 있습니다.)<br><br>
+                                                2. 수집 및 이용목적 : 강사회원과 강의이용자의 원활한 거래 진행, 고객상담, 불만처리 등 민원 처리, 분쟁조정 해결을 위한 기록보존, 위다이브 예약 이용 후 리뷰작성에 따른 위다이브 리워드 지급 및 관련 안내<br><br>
+                                                3. 보관기간<br>
+                                                - 회원탈퇴 시 지체없이 파기<br>
+                                                - 단, 관련 법령에&nbsp;의하여&nbsp;일정&nbsp;기간&nbsp;보관이&nbsp;필요한&nbsp;경우에는&nbsp;해당&nbsp;기간&nbsp;동안&nbsp;보관함<br><br>
+                                                4. 동의 거부권 등에 대한 고지: 정보주체는 개인정보의 수집 및 이용 동의를 거부할 권리가 있으나, 이 경우 상품 및 서비스 예약이 제한될 수 있습니다.&nbsp;그 밖의 내용은 위다이브 개인정보 처리방침을 따릅니다.<br>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+
+                        <div style="position: absolute;bottom: 0;width:100%;">
+                            <a v-on:click="$refs.contentSwiper.$swiper.slideNext();progressBar += 20;" id="btn_next0" href="#" class="btn btn-full font-400 rounded-s shadow-l gradient-highlight color-white bd-w-0 ms-3 me-3 mb-3" style="height: 46px;padding-top: 10px;" disabled="disabled">다음</a>
+                        </div>
+                    </div>
+        </swiper-slide>
+        <swiper-slide
+            :key="1"
+            :virtualIndex="1">
                     <div id="slide1" class="card card-full pb-0 mb-0 border-bottom" style="height: calc( 100vh - 56px );">
                         <div class="content mt-0">
                             <div class="me-3 ms-3 mt-4 font-noto" style="position: relative;">
-                                <span class="font-18 font-500">강습생 연락</span>
+                                <span class="font-18 font-500">강사 연락처</span>
                                 <p class="font-12 color-gray" style="line-height: 1.5;">강습생이 연락할 수 있도록<br/>연락처 정보를 기입해주세요.</p>
                                 <img src="/static/images/assets/gif_hello.gif" height="100" style="position: absolute;top:0;right:0;"/>
                             </div>
@@ -60,8 +113,8 @@
                     </div>
         </swiper-slide>
         <swiper-slide
-            :key="1"
-            :virtualIndex="1">
+            :key="2"
+            :virtualIndex="2">
                     <div id="slide2" class="card card-full pb-0 mb-0 border-bottom" style="height: calc( 100vh - 56px );">
                         <div class="content mt-1">
                             <div class="me-3 ms-3 mt-4 font-noto" style="position: relative;">
@@ -147,8 +200,8 @@
                     </div>
         </swiper-slide>
         <swiper-slide
-            :key="2"
-            :virtualIndex="2">
+            :key="3"
+            :virtualIndex="3">
                 <div id="slide3" class="card card-full pb-0 mb-0 border-bottom" style="height: calc( 100vh - 56px );">
                         <div class="content mt-1">
                             <div class="me-3 ms-3 mt-4 font-noto" style="position: relative;">
@@ -198,8 +251,8 @@
                     </div>
         </swiper-slide>
         <swiper-slide
-            :key="3"
-            :virtualIndex="3">
+            :key="4"
+            :virtualIndex="4">
                 <div id="slide4" class="card card-full pb-0 mb-0 border-bottom" style="min-height: calc( 100vh - 56px );">
                         <div class="content mt-1">
                             <div class="me-3 ms-3 mt-4 font-noto" style="position: relative;">
@@ -260,36 +313,48 @@
                     </div>
         </swiper-slide>
         <swiper-slide
-            :key="4"
-            :virtualIndex="4">
+            :key="5"
+            :virtualIndex="5">
                 <div id="slide5" class="card card-full pb-0 mb-0 border-bottom" style="min-height: calc( 100vh - 56px );">
                         <div class="content mt-1">
                             <div class="me-3 ms-3 mt-4 font-noto" style="position: relative;">
                                 <span class="font-18 font-500">판매 교육 상품</span>
                                 <p class="font-12 color-gray" style="line-height: 1.5;">진행하는 교육상품을 추가하세요.<br/>예) Advance코스, 300,000원.</p>
-                                <img src="/static/images/assets/location-pin.png" height="100" style="position: absolute;top:0;right:0;border-radius:8px;"/>
+                                <img src="/static/images/assets/product.png" height="100" style="position: absolute;top:0;right:0;border-radius:8px;"/>
                             </div>
                             <div class="me-2 ms-2 mt-5 rounded-s" style="background:#efefef;">
-                                <div class="p-3 pt-1 rounded-s">
-                                    <label class="color-gray font-600 font-10" style="background: transparent;">교육상품</label>
-                                    <div class="m-2 pb-4">
-                                        <div v-for="location in locationSelectedList" class=" border-bottom pt-2 pb-2">
-                                            <div class="d-flex align-items-center" style="position:relative !important;">
-                                                <span class="ml-4" style="">
-                                                    <p class="font-noto font-16 mb-0">{{ location }}</p>
-                                                </span>
-                                                <i v-on:click="removeLocationSelected(location)" class="wedive_icoset wedive_icoset_close" style="position: absolute;right:0px;margin:8px;"></i>
-                                            </div>
+                                <draggable :list="education_list" group="backgroundItems" @start="drag=true" @end="drag=false">
+                                    <div v-for="(item, index) in education_list" style="cursor: move" :key="index" class="p-3 pb-1 rounded-s">
+                                        <div class="input-style input-style-always-active no-borders no-icon">
+                                            <i v-on:click="removeEducation(index)" class="wedive_icoset wedive_icoset_close" style="position: absolute;right:0px;margin:8px;"></i>
+                                            <input :id="'product_'+index" v-model="item.product" @focus="focusInput($event)" class="form-control font-noto font-18 font-500" placeholder="교육과정" style="background: transparent;border-bottom-width: 0;">
+                                            <input :id="'price_'+index" v-model="item.price" @focus="focusInput($event)" type="number" class="form-control font-noto font-18 font-500" placeholder="가격" style="background: transparent;border: none !important;">
+                                            <label :for="'product_'+index" class="color-gray font-600 font-10" style="background: transparent;">교육과정 {{ (index+1) }}</label>
                                         </div>
                                     </div>
-                                </div>
+                                </draggable>
                             </div>
+                            <div class="text-center mt-2 opacity-50 font-noto" v-on:click="addEducation()"><i class="fas fa-plus me-2"></i> 교육과정 추가</div>
                         </div>
                         <div class="row me-0 ms-0 mb-0" style="position: absolute;bottom: 0;width:100%;padding-left:20px;padding-right:20px;">
                             <a v-on:click="$refs.contentSwiper.$swiper.slidePrev();progressBar -= 20;" class="col-6 btn btn-full font-400 rounded-s shadow-l gradient-highlight color-white bd-w-0 mb-3" style="height: 46px;padding-top: 10px;margin-left:-4px;margin-right:4px;">이전</a>
-                            <a v-on:click="$refs.contentSwiper.$swiper.slideNext();progressBar += 20;" id="btn_next4" href="#" class="col-6 btn btn-full font-400 rounded-s shadow-l gradient-highlight color-white bd-w-0 mb-3 gonext" style="height: 46px;padding-top: 10px;margin-right:-4px;margin-left:4px;" disabled="disabled">다음</a>
+                            <a v-on:click="$refs.contentSwiper.$swiper.slideNext();progressBar += 20;" id="btn_next4" href="#" class="col-6 btn btn-full font-400 rounded-s shadow-l gradient-highlight color-white bd-w-0 mb-3 gonext" style="height: 46px;padding-top: 10px;margin-right:-4px;margin-left:4px;">다음</a>
                         </div>
                     </div>
+        </swiper-slide>
+        <swiper-slide
+            :key="6"
+            :virtualIndex="6">
+                <div id="slide6" class="card card-full pb-0 mb-0 border-bottom" style="min-height: calc( 100vh - 56px );">
+                        <div class="content mt-1 text-center mt-5">
+                            <img class="mt-5" src="/static/images/assets/completed.gif" width="35%" />
+                            <p class="font-noto font-18 mb-1 mt-3">접수가 완료되었습니다.</p>
+                            <p class="font-noto font-14 mb-0 color-gray">위다이브 메니저가 이미지 수정 후 등록이 완료됩니다.</p>
+                        </div>
+                        <div class="row me-0 ms-0 mb-0" style="position: absolute;bottom: 0;width:100%;padding-left:20px;padding-right:20px;">
+                            <a v-on:click="finished();" id="btn_next5" href="#" class="col-12 btn btn-full font-400 rounded-s shadow-l gradient-highlight color-white bd-w-0 mb-3" style="height: 46px;padding-top: 10px;margin-right:-4px;margin-left:4px;">완료</a>
+                        </div>
+                </div>
         </swiper-slide>
         </swiper>
 
@@ -404,6 +469,7 @@
 import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
 import 'swiper/css/swiper.css'
 import { GraphQLClient, request, gql } from "graphql-request";
+import draggable from 'vuedraggable'
 
 const axios = require("axios")
 export default {
@@ -436,6 +502,7 @@ export default {
   components: {
     Swiper,
     SwiperSlide,
+    draggable,
   },
   created() {
     const insname = this.ins_name;
@@ -452,6 +519,7 @@ export default {
   },
   data () {
     return {
+        check_agree: false,
         ins_name: (localStorage.userName ? localStorage.userName : ''),
         ins_phone: '',
         ins_email: (localStorage.userEmail ? localStorage.userEmail : ''),
@@ -517,10 +585,18 @@ export default {
         },
         suggestions : (localStorage.suggestion ? JSON.parse(localStorage.suggestion) : []),
         locationSelectedList: [],
-        progressBar: 20,
+        progressBar: 0,
+        education_list: [{product: '', price: ''}],
     }
   }, 
   watch: {
+      check_agree: function(newVal, oldVal) {
+          if (this.check_agree != false) {
+              $("#btn_next0").attr("disabled", false);
+          } else {
+              $("#btn_next0").attr("disabled", true);
+          }
+      },
       ins_name: function(newVal, oldVal) {
           if (this.ins_name != '' && this.ins_phone != '' && this.ins_email != '') {
               $("#btn_next1").attr("disabled", false);
@@ -617,6 +693,12 @@ export default {
       },
   },
   methods: {
+      removeEducation(index) {
+          this.education_list.splice(index,1);
+      },
+      addEducation() {
+          this.education_list.push({product: '', price: ''});
+      },
       clickLocation(ev) {
           console.log(ev);
           if (this.locationSelectedList.length >= 10) {
@@ -793,6 +875,12 @@ export default {
       },
       focusInput2(event) {
           consoel.log(event);
+      },
+      focusInput3(event) {
+          console.log(event);
+          $(".focusBorder").each(function(index, item){item.classList.remove("focusBorder");});
+          $(".toggleBorder").each(function(index, item){item.classList.remove("toggleBorder");});
+          event.currentTarget.parentNode.parentNode.parentNode.classList.add("focusBorder");
       },
       onSlideChange() {
           //console.log("onSlideChange")
